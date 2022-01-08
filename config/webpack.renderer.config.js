67b7aa38c61c5f6e417749e81290c721230e17a3
tang weiping <weepingtown@qq.com>
2022-01-08 20:59:17 +0800

    move webpack config to config folder

---
+++ config/webpack.renderer.config.js
@@ -0,0 +1,30 @@
+const rules = require('./webpack.rules');
+const fs = require('fs');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+},
+{
+  test: /\.wasm$/,
+  type: "javascript/auto",
+  loader: "file-loader",
+  options: {
+    outputPath: "wasm/"
+  }
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      child_process: false,
+      path: false,
+      crypto: false,
+    }
+  },
+};
